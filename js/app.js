c6a44762c4ee28efe2e8353752c42a854a4a0964
Mohamed Ahmed Abdelkader <madsaaeq@gmail.com>
2021-07-25 04:03:33 +0200

    Init commit.

---
+++ js/app.js
@@ -0,0 +1,74 @@
+/**
+ * ANIMATION:
+ * Hero section animation
+ */
+
+function animateIn(element, timeInterval) {
+	setTimeout( function(){
+		element.classList.add('slide-in');
+	}, timeInterval);
+};
+
+function devicesAnimation(){
+	const STACKS = document.querySelectorAll('[class^=stack--0]');
+	for ( let i = 0; i < STACKS.length; i++ ) {
+		const TOPDEVICE		= STACKS[i].querySelector('.top'),
+			BOTTOMDEVICE	= STACKS[i].querySelector('.bottom');
+		animateIn(TOPDEVICE, (i*240));
+		animateIn(BOTTOMDEVICE, (i*450))
+	}
+};
+
+/**
+ * ANIMATION:
+ * Parallax Effect
+ */
+
+window.addEventListener( 'scroll', function() {
+	let layers, layer, depth, distance, movement, translate3d;
+	distance		= this.pageYOffset;
+	layers 		= document.querySelectorAll('[data-type="parallax"]');
+	for ( let i = 0; i < layers.length; i++ ) {
+	layer 								= layers[i];
+	depth 								= layer.getAttribute('data-depth');
+	movement							= -(distance * depth);
+	translate3d 						= 'translate3d(0, ' + movement + 'px, 0)';
+	layer.style['-webkit-transform'] 	= translate3d;
+	layer.style['-moz-transform'] 		= translate3d;
+	layer.style['-ms-transform'] 		= translate3d;
+	layer.style['-o-transform'] 		= translate3d;
+	layer.style.transform 				= translate3d;
+	}
+});
+
+/**
+ * CAPTURE EVENT:
+ * Capture when feature screen added to stack.
+ */
+
+function stackLayerOpacity() {
+	const LAYERS = document.querySelectorAll('.layer');
+	for ( let i = 1; i < LAYERS.length-1; i++ ) {
+		const CURRENT 	= LAYERS[i],
+			PREVIOUS 	= LAYERS[i-1]
+		if( CURRENT.offsetTop <= PREVIOUS.offsetTop + PREVIOUS.offsetHeight) {
+			PREVIOUS.style.opacity = (
+				Math.abs(
+					((((CURRENT.offsetTop - PREVIOUS.offsetTop)/100)-(i/1000))
+				)/10)
+			);
+		}
+	}
+}
+
+/**
+ * Call Functions
+ */
+
+ window.addEventListener( 'DOMContentLoaded', function(){
+	devicesAnimation();
+});
+
+window.addEventListener( 'scroll', function(){
+	stackLayerOpacity();
+});
